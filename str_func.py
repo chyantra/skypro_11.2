10ce1eda52160db47e0cc6f132930f34e0b6145f
yachumanova <chyantra@gmail.com>
2023-04-23 12:54:02 +0300

    added in file str_func.py funcs capitalize

--- str_func.py
+++ str_func.py
@@ -1,2 +1,9 @@
+
 def upper_text(value):
+    """Функция возвращает текст капсом"""
     return value.upper()
+
+
+def capitalize_text(value):
+    """Функция возвращает текст с каждым словом с большой буквы"""
+    return value.capitalize()
